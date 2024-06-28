1995018b0d05be8571da9dde0d88c0cc8ec8e4fc
lackedn <165629249+lackedn@users.noreply.github.com>
2024-06-28 03:04:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-#Plotter and Sudoku environment by CMPUT 366 Team
-'''class PlotResults:
-    """
-    Class to plot the results. 
-    """
-    def plot_results(self, data1, data2, label1, label2, filename):
-        """
-        This method receives two lists of data point (data1 and data2) and plots
-        a scatter plot with the information. The lists store statistics about individual search 
-        problems such as the number of nodes a search algorithm needs to expand to solve the problem.
 
-        The function assumes that data1 and data2 have the same size. 
-
-        label1 and label2 are the labels of the axes of the scatter plot. 
-        
-        filename is the name of the file in which the plot will be saved.
-        """
-        _, ax = plt.subplots()
-        ax.scatter(data1, data2, s=100, c="g", alpha=0.5, cmap=plt.cm.coolwarm, zorder=10)
-    
-        lims = [
-        np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
-        np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
-        ]
-    
-        ax.plot(lims, lims, 'k-', alpha=0.75, zorder=0)
-        ax.set_aspect('equal')
-        ax.set_xlim(lims)
-        ax.set_ylim(lims)
-        plt.xlabel(label1)
-        plt.ylabel(label2)
-        plt.grid()
-        plt.savefig(filename)
-'''
 class Grid:
-    """
-    Class to represent an assignment of values to the 81 variables defining a Sudoku puzzle. 
-
-    Variable _cells stores a matrix with 81 entries, one for each variable in the puzzle. 
-    Each entry of the matrix stores the domain of a variable. Initially, the domains of variables
-    that need to have their values assigned are 123456789; the other domains are limited to the value
-    initially assigned on the grid. Backtracking search and AC3 reduce the the domain of the variables 
-    as they proceed with search and inference.
-    """
     def __init__(self):
         self._cells = []
         self._complete_domain = "123456789"
         self._width = 9
 
     def copy(self):
-        """
-        Returns a copy of the grid. 
-        """
         copy_grid = Grid()
         copy_grid._cells = [row.copy() for row in self._cells]
         return copy_grid
 
     def get_cells(self):
-        """
-        Returns the matrix with the domains of all variables in the puzzle.
-        """
         return self._cells
 
     def get_width(self):
-        """
-        Returns the width of the grid.
-        """
         return self._width
 
     def read_file(self, string_puzzle):
-        """
-        Reads a Sudoku puzzle from string and initializes the matrix _cells. 
-
-        This is a valid input string:
-
-        4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......
-
-        This is translated into the following Sudoku grid:
-
-        - - - - - - - - - - - - - 
-        | 4 . . | . . . | 8 . 5 | 
-        | . 3 . | . . . | . . . | 
-        | . . . | 7 . . | . . . | 
-        - - - - - - - - - - - - - 
-        | . 2 . | . . . | . 6 . | 
-        | . . . | . 8 . | 4 . . | 
-        | . . . | . 1 . | . . . | 
-        - - - - - - - - - - - - - 
-        | . . . | 6 . 3 | . 7 . | 
-        | 5 . . | 2 . . | . . . | 
-        | 1 . 4 | . . . | . . . | 
-        - - - - - - - - - - - - - 
-        """
         i = 0
         row = []
         for p in string_puzzle:
@@ -107,27 +33,9 @@ class Grid:
                 row = []
             
     def print(self):
-        """
-        Prints the grid on the screen. Example:
-
-        - - - - - - - - - - - - - 
-        | 4 . . | . . . | 8 . 5 | 
-        | . 3 . | . . . | . . . | 
-        | . . . | 7 . . | . . . | 
-        - - - - - - - - - - - - - 
-        | . 2 . | . . . | . 6 . | 
-        | . . . | . 8 . | 4 . . | 
-        | . . . | . 1 . | . . . | 
-        - - - - - - - - - - - - - 
-        | . . . | 6 . 3 | . 7 . | 
-        | 5 . . | 2 . . | . . . | 
-        | 1 . 4 | . . . | . . . | 
-        - - - - - - - - - - - - - 
-        """
         for _ in range(self._width + 4):
             print('-', end=" ")
         print()
-
         for i in range(self._width):
             print('|', end=" ")
             for j in range(self._width):
@@ -147,16 +55,10 @@ class Grid:
         print()
 
     def print_domains(self):
-        """
-        Print the domain of each variable for a given grid of the puzzle.
-        """
         for row in self._cells:
             print(row)
 
     def is_solved(self):
-        """
-        Returns True if the puzzle is solved and False otherwise. 
-        """
         for i in range(self._width):
             for j in range(self._width):
                 if len(self._cells[i][j]) > 1 or not self.is_value_consistent(self._cells[i][j], i, j):
@@ -183,18 +85,10 @@ class Grid:
         return True
 
 class VarSelector:
-    """
-    Interface for selecting variables in a partial assignment. 
-
-    Extend this class when implementing a new heuristic for variable selection.
-    """
     def select_variable(self, grid):
         pass
 
 class FirstAvailable(VarSelector):
-    """
-    Naïve method for selecting variables; simply returns the first variable encountered whose domain is larger than one.
-    """
     def select_variable(self, grid):
         for i in range(grid.get_width()):
             for j in range(grid.get_width()):
@@ -202,9 +96,6 @@ class FirstAvailable(VarSelector):
                     return(i,j)
 
 class MRV(VarSelector):
-    """
-    Implements the MRV heuristic, which returns one of the variables with smallest domain. 
-    """
     def select_variable(self, grid):
         current_i = None
         current_j = None
@@ -218,16 +109,8 @@ class MRV(VarSelector):
         return(current_i, current_j)
 
 class AC3:
-    """
-    This class implements the methods needed to run AC3 on Sudoku. 
-    """
     def remove_domain_row(self, grid, row, column):
-        """
-        Given a matrix (grid) and a cell on the grid (row and column) whose domain is of size 1 (i.e., the variable has its
-        value assigned), this method removes the value of (row, column) from all variables in the same row. 
-        """
         variables_assigned = []
-
         for j in range(grid.get_width()):
             if j != column:
                 new_domain = grid.get_cells()[row][j].replace(grid.get_cells()[row][column], '')
@@ -240,10 +123,6 @@ class AC3:
         return variables_assigned, False
 
     def remove_domain_column(self, grid, row, column):
-        """
-        Given a matrix (grid) and a cell on the grid (row and column) whose domain is of size 1 (i.e., the variable has its
-        value assigned), this method removes the value of (row, column) from all variables in the same column. 
-        """
         variables_assigned = []
         for j in range(grid.get_width()):
             if j != row:
@@ -257,10 +136,6 @@ class AC3:
         return variables_assigned, False
 
     def remove_domain_unit(self, grid, row, column):
-        """
-        Given a matrix (grid) and a cell on the grid (row and column) whose domain is of size 1 (i.e., the variable has its
-        value assigned), this method removes the value of (row, column) from all variables in the same unit. 
-        """
         variables_assigned = []
         row_init = (row // 3) * 3
         column_init = (column // 3) * 3
@@ -298,13 +173,7 @@ class AC3:
         return grid
 
 class Backtracking:
-    """
-    Class that implements backtracking search for solving CSPs. 
-    """
     def search(self, grid, var_selector, already_preinitialized = None):
-        """
-        Implements backtracking search with inference. 
-        """
         ac3 = AC3()
         if already_preinitialized is None:
             #initialize partially filled sudoku board with AC3 to find possible candidates
@@ -353,8 +222,6 @@ def main():
         mrv_search.print()
         print('Solved board (FA):')
         fa_search.print()
-    #plotter = PlotResults()
-    #plotter.plot_results(mrv_time, first_time, "Running Time Backtracking (MRV)", "Running Time Backtracking (FA)", 'res.png')
     
 if __name__ == '__main__':
     main()
